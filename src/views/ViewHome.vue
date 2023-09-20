903ea76dd3ac9f582ad330d9afe5adc3d83c95d1
botmaster <pascal.achard@gmail.com>
2023-09-20 17:22:21 +0200

    feat(deps): add @vueuse/core

--- src/views/ViewHome.vue
+++ src/views/ViewHome.vue
@@ -1,7 +1,14 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import {useMouse} from '@vueuse/core'
+
+// tracks mouse position
+const {x, y} = useMouse()
+
+</script>
 
 <template>
   <div class="">
     <h2>View Home</h2>
+    <pre>Mouse position: {{ x }}, {{ y }}</pre>
   </div>
 </template>
