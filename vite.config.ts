91977d451fe8df54dbb92c33aa779209c54a931c
botmaster <pascal.achard@gmail.com>
2023-09-21 11:13:00 +0200

    feat(deps): Add vite-plugin-vue-devtools

--- vite.config.ts
+++ vite.config.ts
@@ -2,10 +2,11 @@ import { URL, fileURLToPath } from 'node:url';
 import { defineConfig } from 'vite';
 import vue from '@vitejs/plugin-vue';
 import svgLoader from 'vite-svg-loader';
+import VueDevTools from 'vite-plugin-vue-devtools';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue(), svgLoader()],
+  plugins: [vue(), svgLoader(), VueDevTools()],
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url)),
