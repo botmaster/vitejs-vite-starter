69846e31dfe174a6cd9ace21a82aeae0368b9673
botmaster <pascal.achard@gmail.com>
2023-10-18 00:23:31 +0200

    feat: Add AppAlert specs

---
+++ src/components/__tests__/AppAlert.spec.ts
@@ -0,0 +1,74 @@
+import { describe, expect, test } from 'vitest';
+import { mount } from '@vue/test-utils';
+import AppAlert from '../shared/AppAlert.vue';
+
+
+describe('Alert component', () => {
+  test('renders the correct style for default', () => {
+    const wrapper = mount(AppAlert, {});
+
+    expect(wrapper.classes()).toEqual(expect.arrayContaining(['alert--is-default']));
+  });
+
+  test('renders the correct style for info', () => {
+    const status = 'info';
+    const wrapper = mount(AppAlert, {
+      props: { status },
+    });
+
+    expect(wrapper.classes()).toEqual(expect.arrayContaining(['alert--is-info']));
+  });
+
+  test('renders the correct style for warning', () => {
+    const status = 'warning';
+    const wrapper = mount(AppAlert, {
+      props: { status },
+    });
+
+    expect(wrapper.classes()).toEqual(expect.arrayContaining(['alert--is-warning']));
+  });
+
+  test('renders the correct style for success', () => {
+    const status = 'success';
+    const wrapper = mount(AppAlert, {
+      props: { status },
+    });
+
+    expect(wrapper.classes()).toEqual(expect.arrayContaining(['alert--is-success']));
+  });
+
+  test('renders the correct style for danger', () => {
+    const status = 'danger';
+    const wrapper = mount(AppAlert, {
+      props: { status },
+    });
+
+    expect(wrapper.classes()).toEqual(expect.arrayContaining(['alert--is-danger']));
+  });
+
+  test('is closable', async () => {
+    const wrapper = mount(AppAlert, {
+      props: { closable: true },
+    });
+
+    expect(wrapper.find('.alert__close').exists()).toBe(true);
+  });
+
+  test('is not closable', async () => {
+    const wrapper = mount(AppAlert, {
+      props: { closable: false },
+    });
+
+    expect(wrapper.find('.alert__close').exists()).toBe(false);
+  });
+
+  test('no icon if no status', async () => {
+    const wrapper = mount(AppAlert, {});
+
+    expect(wrapper.find('.alert__icon').exists()).toBe(false);
+  });
+});
+
+
+
+
